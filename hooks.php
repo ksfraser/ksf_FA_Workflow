82416a1759bdea321dccd6d7943c445858dd831e
Kevin Fraser <kevin@ksfraser.com>
2026-05-15 08:01:00 -0600

    fix: add version property and composer install on activation

--- hooks.php
+++ hooks.php
@@ -7,6 +7,7 @@ define('SS_WORKFLOW', 143 << 8);
 
 class hooks_fa_workflow extends hooks {
     var $module_name = 'fa_workflow';
+    var $version = '1.0.0';
 
     function install_options($app) {
         global $path_to_root;
